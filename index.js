5702fecc9caa398007cfb8cd942ac4e2cc3a60d4
“Flavio <samus.sp@gmail.com>
2022-12-07 22:44:16 -0300

    consumindo detalhes da API

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
- function convertPokemonToLi(pokemon) {
+function convertPokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes.map(
+    (typeslot) => `<li class="type">${typeslot.type.name}</li>`
+  )
+}
+
+function convertPokemonToLi(pokemon) {
   return `
         <li class="Pokemon">
-            <span class="number">#001</span>
+            <span class="number">#${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
+           
             <div class="detail">
                 <ol class="types">
-                     <li class="type">grass</li>
-                    <li class="type">poison</li>
+                
+                    ${convertPokemonTypesToLi(pokemon.types).join('') }
                 </ol>
                 <img
-                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    src="${pokemon.sprites.other.dream_world.front_default}"
                     alt="${pokemon.name}"
                 />
             </div>
         </li>
     `;
 }
-const pokemonList = document.getElementById('pokemonList')
+const pokemonList = document.getElementById("pokemonList");
 pokeApi.getPokemons().then((pokemons = []) => {
-    const NewHtml = pokemons.map(convertPokemonToLi).join('')
-    pokemonList.innerHTML = NewHtml
-    // pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')            
-        /* otimizando o codigo
+  const NewHtml = pokemons.map(convertPokemonToLi).join("");
+  pokemonList.innerHTML = NewHtml;
+  // pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+  /* otimizando o codigo
     const newList = pokemons.map((pokemon) => {
         return convertPokemonToLi (pokemon)
     })
     const NewHtml = newList.join('')
     pokemonList.innerHTML += NewHtml */
-  
-            // subistituindo por map
-    /*  const listItens = []
+
+  // subistituindo por map
+  /*  const listItens = []
    for (let index = 0; index < pokemons.length; index++) {
       const pokemon = pokemons[index];
       listItens.push(convertPokemonToLi(pokemon))      
     }
   })  */
- /*/ .then(function (response) {
+  /*/ .then(function (response) {
         console.log(response)
     })
     .catch(function (error) {
@@ -43,5 +50,4 @@ pokeApi.getPokemons().then((pokemons = []) => {
     })
     .finally(function () {
         console.log('requisição concluida!'); */
-    }) 
-    
+});
--- poke-Api.js
+++ poke-Api.js
@@ -1,8 +1,26 @@
  const pokeApi = {}
+
+ pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((Response) => Response.json())
+ } 
+  
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((Response) => Response.json())
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
         .catch((error) => console.error(error))
 }
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4'),
+    fetch('https://pokeapi.co/api/v2/pokemon/5')
+]).then((results) => {
+    console.log(results)
+})
